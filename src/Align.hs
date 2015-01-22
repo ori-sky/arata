7b4354916836faeedf1aaeb1d77e4a4f02d89090
Shockk <shokku.ra@gmail.com>
2015-01-22 21:32:20 +0000

    Update copyright notice years

--- src/Align.hs
+++ src/Align.hs
@@ -1,4 +1,4 @@
-{- Copyright 2014 David Farrell <shokku.ra@gmail.com>
+{- Copyright 2014-2015 David Farrell <shokku.ra@gmail.com>
 
  - Licensed under the Apache License, Version 2.0 (the "License");
  - you may not use this file except in compliance with the License.
